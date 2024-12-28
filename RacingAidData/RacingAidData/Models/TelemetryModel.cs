3049a8b4e42a45e6eb953b2c00a7472b70fd6613
Luke Gregory <L.Gregory11@hotmail.co.uk>
2024-12-28 01:05:27 +0000

    Added TelemetryModel

---
+++ RacingAidData/RacingAidData/Models/TelemetryModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RacingAidData.Models;
+
+/// <summary>
+/// A Standard telemetry data model
+/// </summary>
+public struct TelemetryModel
+{
+    public float SpeedMetresPerSecond { get; private set; }
+    
+    public float ThrottlePercentage { get; private set; }
+    
+    public float BrakePercentage { get; private set; }
+    
+    public float SteeringAngle { get; private set; }
+    
+    public float Rpm { get; private set; }
+    
+    public short Gear { get; private set; }
+}
