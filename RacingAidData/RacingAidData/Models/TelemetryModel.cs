13054acc71f8b32770af7b26e75c03df9cb30743
Luke Gregory <L.Gregory11@hotmail.co.uk>
2024-12-28 01:16:22 +0000

    Minor adjustments to TelemetryModel

--- RacingAidData/RacingAidData/Models/TelemetryModel.cs
+++ RacingAidData/RacingAidData/Models/TelemetryModel.cs
@@ -5,15 +5,22 @@
 /// </summary>
 public struct TelemetryModel
 {
-    public float SpeedMetresPerSecond { get; private set; }
+    public float SpeedMetresPerSecond { get; init; }
     
-    public float ThrottlePercentage { get; private set; }
+    public float ThrottlePercentage { get; init; }
     
-    public float BrakePercentage { get; private set; }
+    public float BrakePercentage { get; init; }
     
-    public float SteeringAngle { get; private set; }
+    /// <summary>
+    /// The steering angle in degrees. Negative steering angle = counter-clockwise rotation
+    /// </summary>
+    public float SteeringAngleDegrees { get; init; }
     
-    public float Rpm { get; private set; }
+    public float Rpm { get; init; }
     
-    public short Gear { get; private set; }
+    
+    /// <summary>
+    /// The gear. 0 = Neutral, -1 = Reverse
+    /// </summary>
+    public short Gear { get; init; }
 }
