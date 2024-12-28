3f1c6ae5daabf495edba96c3c37d5ab5bcbde0be
Luke Gregory <L.Gregory11@hotmail.co.uk>
2024-12-28 00:59:20 +0000

    Added simple DriverModel

---
+++ RacingAidData/RacingAidData/Models/DriverModel.cs
@@ -0,0 +1,32 @@
+﻿namespace RacingAidData.Models;
+
+public class DriverModel
+{
+    /// <summary>
+    /// Driver name
+    /// </summary>
+    public string FullName { get; private set; }
+    
+    /// <summary>
+    /// The skill rating
+    /// </summary>
+    public string SkillRating { get; private set; }
+    
+    /// <summary>
+    /// The safety rating
+    /// </summary>
+    public string SafetyRating { get; private set; }
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    /// <param name="fullName">The driver's full name</param>
+    /// <param name="skillRating">The driver's skill rating</param>
+    /// <param name="safetyRating">The driver's safety rating</param>
+    public DriverModel(string fullName, string skillRating, string safetyRating)
+    {
+        FullName = fullName;
+        SkillRating = skillRating;
+        SafetyRating = safetyRating;
+    }
+}
