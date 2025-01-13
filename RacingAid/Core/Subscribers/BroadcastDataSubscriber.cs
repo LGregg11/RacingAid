d3d89f1b665c9853071397cb122f963d517a0f12
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-13 16:34:45 +0000

    Added ISubscribeData and broadcast data subscriber

---
+++ RacingAid/Core/Subscribers/BroadcastDataSubscriber.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Sockets;
+using RacingAid.Core.Client;
+using RacingAid.Core.Events;
+
+namespace RacingAid.Core.Subscribers;
+
+public class BroadcastDataSubscriber : ISubscribeData
+{
+    private readonly IDataClient dataClient;
+    private Thread? listenerThread;
+    private bool keepThreadRunning;
+
+    public BroadcastDataSubscriber(IDataClient dataClient)
+    {
+        this.dataClient = dataClient;
+    }
+
+    public event EventHandler<DataReceivedEventArgs>? DataReceived;
+
+    public bool IsSubscribed => listenerThread is { IsAlive: true };
+
+    public void Start()
+    {
+        if (IsSubscribed)
+            return;
+
+        dataClient.Start();
+        StartThread();
+    }
+
+    public void Stop()
+    {
+        if (!IsSubscribed)
+            return;
+
+        dataClient.Stop();
+        StopThread();
+    }
+
+    private void DataSubscriber()
+    {
+        while (keepThreadRunning)
+        {
+            try
+            {
+                byte[]? receiveBytes = dataClient?.Receive();
+                if (receiveBytes is { Length: > 0 })
+                    DataReceived?.Invoke(this, new DataReceivedEventArgs(receiveBytes));
+            }
+            catch (SocketException)
+            {
+                // Client closed - stop the thread
+                keepThreadRunning = false;
+            }
+        }
+    }
+
+    private void StartThread()
+    {
+        keepThreadRunning = true;
+
+        listenerThread = new Thread(DataSubscriber) { Name = $"{nameof(BroadcastDataSubscriber)} Thread" };
+        listenerThread.Start();
+    }
+
+    private void StopThread()
+    {
+        keepThreadRunning = false;
+        listenerThread?.Join();
+    }
+}
---
+++ RacingAid/Core/Subscribers/ISubscribeData.cs
@@ -0,0 +1,13 @@
+﻿using RacingAid.Core.Events;
+
+namespace RacingAid.Core.Subscribers;
+
+public interface ISubscribeData
+{
+    event EventHandler<DataReceivedEventArgs>? DataReceived;
+    
+    bool IsSubscribed { get; }
+    
+    void Start();
+    void Stop();
+}
