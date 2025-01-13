cb26d212087eeaeee7c831ce7549e0f52e8357b2
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-13 23:38:11 +0000

    Added iRacingDataSubscriber

---
+++ RacingAid/Simulators/iRacing/iRacingDataSubscriber.cs
@@ -0,0 +1,41 @@
+﻿using IRSDKSharper;
+using RacingAid.Core.Subscribers;
+
+namespace RacingAid.Simulators.iRacing;
+
+public class iRacingDataSubscriber : ISubscribeData<IRacingSdkData>
+{
+    private readonly IRacingSdk iRacingSdk;
+    
+    public event Action DataReceived;
+
+    public IRacingSdkData LatestData { get; private set; }
+
+    public bool IsSubscribed => iRacingSdk.IsStarted;
+
+    public iRacingDataSubscriber()
+    {
+        iRacingSdk = new IRacingSdk
+        {
+            UpdateInterval = 30
+        };
+
+        iRacingSdk.OnTelemetryData += OnTelemetryData;
+    }
+
+    public void Start()
+    {
+        iRacingSdk.Start();
+    }
+
+    public void Stop()
+    {
+        iRacingSdk.Stop();
+    }
+
+    private void OnTelemetryData()
+    {
+        LatestData = iRacingSdk.Data;
+        DataReceived?.Invoke();
+    }
+}
