c84aec589e5708dbf473c69aa293e4bd47e8288f
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-13 16:33:17 +0000

    Added DataReceivedEventArgs

---
+++ RacingAid/Core/Events/DataReceivedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace RacingAid.Core.Events;
+
+public class DataReceivedEventArgs : EventArgs
+{
+    public byte[] Message { get; init; }
+
+    public DataReceivedEventArgs(byte[] message)
+    {
+        Message = message;
+    }
+}
