4be2675193a8fc8c16bf3885cc20755494e78fa6
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-17 18:11:11 +0000

    Added basic extensions

---
+++ RacingAidWpf/Extensions/TelemetryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace RacingAidWpf.Extensions;
+
+public static class TelemetryExtensions
+{
+    private const float MetresPerSecondToKph = 3.6f;
+
+    public static float ToKph(this float speedMetresPerSecond) =>
+        Convert.ToInt32(speedMetresPerSecond * MetresPerSecondToKph);
+
+    public static string ToGearString(this int gear)
+    {
+        return gear switch
+        {
+            -1 => "R",
+            0 => "N",
+            _ => gear.ToString()
+        };
+    }
+}
