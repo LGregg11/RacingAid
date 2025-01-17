5a4835c67e20f3f6edee5a3ee18c9867589052e4
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-17 18:10:08 +0000

    Added Converters so properties can be stored as their real values (not strings)

---
+++ RacingAidWpf/Converters/PercentageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace RacingAidWpf.Converters;
+
+[ValueConversion(typeof(float), typeof(string))]
+public class PercentageConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is float f)
+            return $"{f*100f:F1}";
+
+        return "N/A";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
---
+++ RacingAidWpf/Converters/SteeringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace RacingAidWpf.Converters;
+
+[ValueConversion(typeof(float), typeof(string))]
+public class SteeringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not float degrees)
+            return "N/A";
+
+        var direction = degrees < 0 ? "R" : "L";
+        var degreesAbs = MathF.Abs(degrees);
+
+        return $"{degreesAbs:F1}{direction}";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
