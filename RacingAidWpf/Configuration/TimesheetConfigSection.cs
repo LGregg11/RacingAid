39422c30b75cffcbc8c66e04078d37c6930fedc9
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-19 20:21:24 +0000

    Added Timesheet config section

---
+++ RacingAidWpf/Configuration/TimesheetConfigSection.cs
@@ -0,0 +1,21 @@
+﻿namespace RacingAidWpf.Configuration;
+
+public class TimesheetConfigSection : ConfigSection
+{
+    public bool CarNumberColumnVisibility { get; private init; } = true;
+    public bool SafetyColumnVisibility { get; private init; } = true;
+    public bool SkillColumnVisibility { get; private init; } = true;
+    public bool LastLapColumnVisibility { get; private init; } = true;
+    public bool FastestLapColumnVisibility { get; private init; } = true;
+    public bool LeaderColumnVisibility { get; private init; } = true;
+    
+    public TimesheetConfigSection(IConfig config) : base(config, "Timesheet")
+    {
+        CarNumberColumnVisibility = GetValue(nameof(CarNumberColumnVisibility), CarNumberColumnVisibility);
+        SafetyColumnVisibility = GetValue(nameof(SafetyColumnVisibility), SafetyColumnVisibility);
+        SkillColumnVisibility = GetValue(nameof(SkillColumnVisibility), SkillColumnVisibility);
+        LastLapColumnVisibility = GetValue(nameof(LastLapColumnVisibility), LastLapColumnVisibility);
+        FastestLapColumnVisibility = GetValue(nameof(FastestLapColumnVisibility), FastestLapColumnVisibility);
+        LeaderColumnVisibility = GetValue(nameof(LeaderColumnVisibility), LeaderColumnVisibility);
+    }
+}
