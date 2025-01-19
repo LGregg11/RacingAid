38790ed16b6d70fdcd16083de5a5b719c2bdd133
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-19 20:21:07 +0000

    Added ConfigSection

---
+++ RacingAidWpf/Configuration/ConfigSection.cs
@@ -0,0 +1,20 @@
+﻿namespace RacingAidWpf.Configuration;
+
+public abstract class ConfigSection(IConfig config, string section)
+{
+    protected T? GetValue<T>(string key, T defaultValue)
+    {
+        if (!config.TryGetValue(section, key, out T? value))
+        {
+            SetValue(key, defaultValue);
+            value = defaultValue;
+        }
+
+        return value;
+    }
+
+    protected bool SetValue<T>(string key, T? value)
+    {
+        return config.SetValue(section, key, value);
+    }
+}
