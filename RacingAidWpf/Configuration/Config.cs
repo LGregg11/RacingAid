5648600f89457b879deeb7e2c62cf2878cbc5226
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-19 20:20:59 +0000

    Added IConfig implementation

---
+++ RacingAidWpf/Configuration/Config.cs
@@ -0,0 +1,21 @@
+﻿using Salaros.Configuration;
+
+namespace RacingAidWpf.Configuration;
+
+public class Config(string path) : IConfig
+{
+    private readonly ConfigParser configParser = new(path);
+
+    public bool TryGetValue<T>(string section, string key, out T? value)
+    {
+        value = default;
+        if (configParser.GetValue(section, key) is not T configValue)
+            return false;
+        
+        value = configValue;
+        return true;
+    }
+
+    public bool SetValue<T>(string section, string key, T value) =>
+        value?.ToString() is { } stringValue && configParser.SetValue(section, key, stringValue);
+}
---
+++ RacingAidWpf/Configuration/IConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace RacingAidWpf.Configuration;
+
+public interface IConfig
+{
+    public bool TryGetValue<T>(string section, string key, out T? value);
+    public bool SetValue<T>(string section, string key, T value);
+}
