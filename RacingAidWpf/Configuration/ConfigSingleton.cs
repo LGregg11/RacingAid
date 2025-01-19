186ec756b5ae937a2f145a84f1722d6aa3832e25
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-19 20:25:29 +0000

    Change ConfigSingleton to use the Config class...

--- RacingAidWpf/Configuration/ConfigSingleton.cs
+++ RacingAidWpf/Configuration/ConfigSingleton.cs
@@ -8,6 +8,6 @@ public static class ConfigSingleton
 {
     private static readonly string ConfigFilePath = Path.Combine(Application.ResourceAssembly.Location, "config.ini");
     
-    private static ConfigParser? configParser;
-    public static ConfigParser ConfigParser => configParser ??= new ConfigParser(ConfigFilePath);
+    private static Config? config;
+    public static Config Config => config ??= new Config(ConfigFilePath);
 }
