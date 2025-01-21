dfa4b9b9c7fb5559fe543988f97d8152fa6272a5
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-21 00:17:37 +0000

    Added steering wheel png

---
+++ RacingAidWpf/Resources/Resource.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace RacingAidWpf.Resources;
+
+public static class Resource
+{
+    private static readonly string AssemblyPath =
+        $"pack://application:,,,/{Assembly.GetExecutingAssembly().GetName().Name};component/Resources";
+    
+    public static Uri SteeringWheelUri => GetImageUri("SteeringWheel.png");
+
+    public static Uri GetImageUri(string imageName) => new($"{AssemblyPath}/Images/{imageName}");
+}
