53962354d92dea8d8dfe2eba64c4462a74c34862
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-21 14:44:58 +0000

    Added screen detector class to control which monitor a window is loaded on

---
+++ RacingAidWpf/ScreenManagement/ScreenDetector.cs
@@ -0,0 +1,25 @@
+﻿using RacingAidWpf.Configuration;
+
+namespace RacingAidWpf.ScreenManagement;
+
+/// <summary>
+/// Use this class to determine which monitor to use and where to place windows
+/// </summary>
+public class ScreenDetector
+{
+    private static readonly GeneralConfigSection GeneralConfigSection = ConfigSectionSingleton.GeneralSection;
+    
+    private int primaryScreenIndex;
+
+    public ScreenDetector()
+    {
+        primaryScreenIndex = GeneralConfigSection.PrimaryMonitor - 1;
+
+        GeneralConfigSection.ConfigUpdated += OnConfigUpdated;
+    }
+
+    private void OnConfigUpdated()
+    {
+        primaryScreenIndex = GeneralConfigSection.PrimaryMonitor - 1;
+    }
+}
