56630d38bcca0072070d3c4f9bf23c2a95478986
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-22 00:21:39 +0000

    Refactored the resource class about a bit

--- RacingAidWpf/Resources/Resource.cs
+++ RacingAidWpf/Resources/Resource.cs
@@ -1,15 +1,19 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
 
 namespace RacingAidWpf.Resources;
 
 public static class Resource
 {
-    private static readonly string AssemblyPath =
-        $"pack://application:,,,/{ExecutingAssembly.GetName().Name};component/Resources";
-    
-    public static Assembly ExecutingAssembly => Assembly.GetExecutingAssembly();
+    public static string ExecutingDirectory =>
+        Path.GetDirectoryName(ExecutingAssembly.Location) ?? Directory.GetCurrentDirectory();
     
     public static Uri SteeringWheelUri => GetImageUri("SteeringWheel.png");
+    
+    private static readonly string AssemblyPath =
+        $"pack://application:,,,/{ExecutingAssembly.GetName().Name};component/Resources";
 
-    public static Uri GetImageUri(string imageName) => new($"{AssemblyPath}/Images/{imageName}");
+    private static Assembly ExecutingAssembly => Assembly.GetExecutingAssembly();
+
+    private static Uri GetImageUri(string imageName) => new($"{AssemblyPath}/Images/{imageName}");
 }
