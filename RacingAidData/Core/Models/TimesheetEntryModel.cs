fa772bf3b1a1164644cfe59914ffc9971f805017
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-23 19:39:31 +0000

    Added generic TimingEntryModel, inherited in TImesheetEntryModel

--- RacingAidData/Core/Models/TimesheetEntryModel.cs
+++ RacingAidData/Core/Models/TimesheetEntryModel.cs
@@ -1,64 +1,9 @@
 ﻿namespace RacingAidData.Core.Models;
 
-public class TimesheetEntryModel
+public class TimesheetEntryModel : TimingEntryModel
 {
-    /// <summary>
-    /// Overall position
-    /// </summary>
-    public int OverallPosition { get; init; }
-    
-    /// <summary>
-    /// Class position
-    /// </summary>
-    public int ClassPosition { get; init; }
-    
-    /// <summary>
-    /// Driver name
-    /// </summary>
-    public string FullName { get; init; }
-    
-    /// <summary>
-    /// The skill rating
-    /// </summary>
-    public string SkillRating { get; init; }
-    
-    /// <summary>
-    /// The safety rating
-    /// </summary>
-    public string SafetyRating { get; init; }
-    
-    /// <summary>
-    /// The model of the car the driver is racing
-    /// </summary>
-    public string CarModel { get; init; }
-    
-    /// <summary>
-    /// The number the car is racing under
-    /// </summary>
-    public int CarNumber { get; init; }
-    
-    /// <summary>
-    /// Last lap time in milliseconds
-    /// </summary>
-    public int LastLapMs { get; init; }
-    
-    /// <summary>
-    /// Fastest lap time in milliseconds
-    /// </summary>
-    public int FastestLapMs { get; init; }
-    
     /// <summary>
     /// Gap to leader in milliseconds
     /// </summary>
-    public int GapToLeaderMs { get; set; }
-    
-    /// <summary>
-    /// Laps driven
-    /// </summary>
-    public int LapsDriven { get; init; }
-    
-    /// <summary>
-    /// Whether this entry is of the local user
-    /// </summary>
-    public bool IsLocal { get; init; }
+    public int GapToLeaderMs { get; init; }
 }
