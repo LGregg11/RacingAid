3be55a2a09bcf0d1c7537e4399bc5fb6dbec3ec9
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-23 19:43:23 +0000

    Added relative overlay

---
+++ RacingAidWpf/View/RelativeOverlay.xaml.cs
@@ -0,0 +1,14 @@
+﻿using RacingAidWpf.ViewModel;
+
+namespace RacingAidWpf.View;
+
+public partial class RelativeOverlay
+{
+    private readonly RelativeOverlayViewModel relativeOverlayViewModel = new();
+    
+    public RelativeOverlay()
+    {
+        InitializeComponent();
+        DataContext = relativeOverlayViewModel;
+    }
+}
---
+++ RacingAidWpf/ViewModel/RelativeOverlayViewModel.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using RacingAidData.Core.Models;
+using RacingAidWpf.Configuration;
+using RacingAidWpf.Model;
+
+namespace RacingAidWpf.ViewModel;
+
+public class RelativeOverlayViewModel : NotifyPropertyChanged
+{
+    private ObservableCollection<RelativeGridRow> relative = [];
+    public ObservableCollection<RelativeGridRow> Relative
+    {
+        get => relative;
+        private set
+        {
+            if (relative == value)
+                return;
+            
+            relative = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    #region Visibility properties
+
+    public Visibility CarNumberColumnVisibility => ConfigSectionSingleton.RelativeSection.DisplayCarNumber ? Visibility.Visible : Visibility.Hidden;
+    public Visibility SafetyColumnVisibility => ConfigSectionSingleton.RelativeSection.DisplaySafetyRating ? Visibility.Visible : Visibility.Hidden;
+    public Visibility SkillColumnVisibility => ConfigSectionSingleton.RelativeSection.DisplaySkillRating ? Visibility.Visible : Visibility.Hidden;
+    public Visibility LastLapColumnVisibility => ConfigSectionSingleton.RelativeSection.DisplayLastLap ? Visibility.Visible : Visibility.Hidden;
+    public Visibility FastestLapColumnVisibility => ConfigSectionSingleton.RelativeSection.DisplayFastestLap ? Visibility.Visible : Visibility.Hidden;
+    public Visibility DeltaToLocalColumnVisibility => ConfigSectionSingleton.RelativeSection.DisplayGapToLocal ? Visibility.Visible : Visibility.Hidden;
+    
+    #endregion
+    
+    public RelativeOverlayViewModel()
+    {
+        RacingAidUpdateDispatch.Update += UpdateProperties;
+
+        ConfigSectionSingleton.RelativeSection.ConfigUpdated += OnConfigUpdated;
+    }
+
+    private void UpdateProperties()
+    {
+        UpdateDriversDataGrid();
+    }
+
+    private void UpdateDriversDataGrid()
+    {
+        // We need to order the drivers by their current lap distance and determine the delta from the current (local) driver's lap distance
+        // Because the current driver could be 0.9 (90%) of the way through the lap, or in the pits, and you'd want
+        // to see who is behind/ ahead of you.
+        // i.e. local at 0.9, other at 0.6 -> -0.3 lap distance behind, local at 0.3, other at 0.9 -> -0.4 lap distance behind
+
+        var relativeModel = RacingAidSingleton.Instance.Relative;
+        var relativeModelEntries = relativeModel.Entries;
+
+        if (relativeModelEntries.Count == 0)
+        {
+            Relative = [];
+            return;
+        }
+        
+        var currentDriver = relativeModel.LocalEntry ?? relativeModelEntries.First();
+        var newRelativeGridRows = CreateOrderedRelativeGridRowsByLapDistance(relativeModelEntries, currentDriver);
+        
+        const int maxEntriesToDisplayAheadOrBehind = 3;
+        var currentDriverIndex = newRelativeGridRows.FindIndex(r => r.CarNumber == currentDriver.CarNumber);
+        var minEntryIndex = Math.Max(currentDriverIndex - maxEntriesToDisplayAheadOrBehind, 0);
+        var maxEntryIndex = Math.Min(currentDriverIndex + maxEntriesToDisplayAheadOrBehind, newRelativeGridRows.Count - 1);
+
+        var relativeGridRowsToDisplay = newRelativeGridRows.GetRange(minEntryIndex, maxEntryIndex - minEntryIndex);
+
+        Relative = new ObservableCollection<RelativeGridRow>(relativeGridRowsToDisplay);
+    }
+
+    private void OnConfigUpdated()
+    {
+        OnPropertyChanged(nameof(CarNumberColumnVisibility));
+        OnPropertyChanged(nameof(SafetyColumnVisibility));
+        OnPropertyChanged(nameof(SkillColumnVisibility));
+        OnPropertyChanged(nameof(LastLapColumnVisibility));
+        OnPropertyChanged(nameof(FastestLapColumnVisibility));
+        OnPropertyChanged(nameof(DeltaToLocalColumnVisibility));
+    }
+
+    private List<RelativeGridRow> CreateOrderedRelativeGridRowsByLapDistance(List<RelativeEntryModel> relativeModelEntries, RelativeEntryModel currentDriver)
+    {
+        const float centerPercentage = 0.5f;
+        var lapDistancePercentageDelta = currentDriver.LapDistancePercentage - centerPercentage;
+        
+        var updatedRelativeModeEntries = new List<RelativeGridRow>();
+        
+        foreach (var relativeModelEntry in relativeModelEntries)
+        {
+            var updatedLapDistancePercentage = relativeModelEntry.LapDistancePercentage - lapDistancePercentageDelta;
+            switch (updatedLapDistancePercentage)
+            {
+                case < 0:
+                    updatedLapDistancePercentage += 1;
+                    break;
+                case > 1:
+                    updatedLapDistancePercentage -= 1;
+                    break;
+            }
+
+            var relativeGridRow = RelativeModelToGridRow(relativeModelEntry);
+            relativeGridRow.LapDistancePercentage = updatedLapDistancePercentage;
+            
+            updatedRelativeModeEntries.Add(relativeGridRow);
+        }
+
+        return updatedRelativeModeEntries.OrderByDescending(e => e.LapDistancePercentage).ToList();
+    }
+
+    private static RelativeGridRow RelativeModelToGridRow(RelativeEntryModel relativeEntryModel)
+    {
+        return new RelativeGridRow(
+            relativeEntryModel.OverallPosition,
+            relativeEntryModel.ClassPosition,
+            relativeEntryModel.FullName,
+            relativeEntryModel.SkillRating,
+            relativeEntryModel.SafetyRating,
+            relativeEntryModel.CarModel,
+            relativeEntryModel.CarNumber,
+            relativeEntryModel.LastLapMs,
+            relativeEntryModel.FastestLapMs,
+            relativeEntryModel.GapToLocalMs,
+            relativeEntryModel.LapDistancePercentage,
+            relativeEntryModel.IsLocal,
+            false);
+    }
+}
