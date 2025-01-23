bf239fc9ab487e5fd205c425a0c9548067ef6a5c
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-23 19:39:45 +0000

    Added Relative timesheet models

---
+++ RacingAidData/Core/Models/RelativeEntryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RacingAidData.Core.Models;
+
+public class RelativeEntryModel : TimingEntryModel
+{
+    /// <summary>
+    /// Gap to local driver (on track) in milliseconds
+    /// </summary>
+    public int GapToLocalMs { get; init; }
+    
+    /// <summary>
+    /// 0-1 -> 0-100%
+    /// </summary>
+    public float LapDistancePercentage { get; init; }
+}
---
+++ RacingAidData/Core/Models/RelativeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RacingAidData.Core.Models;
+
+public class RelativeModel : RaceDataModel
+{
+    public RelativeEntryModel? LocalEntry { get; init; }
+
+    public List<RelativeEntryModel> Entries { get; init; } = [];
+}
