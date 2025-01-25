0db9fda8ceb799feaa0f981810356134850ef4b1
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-25 20:35:29 +0000

    Added IHandleData and JsonHandler implementation

---
+++ RacingAidWpf/FileHandlers/IHandleData.cs
@@ -0,0 +1,7 @@
+﻿namespace RacingAidWpf.FileHandlers;
+
+public interface IHandleData<T> where T : class
+{
+    public bool TryDeserializeFromFile(string filePath, out T? data);
+    public bool TrySerializeToFile(string filePath, T data);
+}
---
+++ RacingAidWpf/FileHandlers/JsonHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace RacingAidWpf.FileHandlers;
+
+public class JsonHandler<T> : IHandleData<T> where T : class
+{
+    public bool TryDeserializeFromFile(string filePath, out T? data)
+    {
+        data = null;
+
+        if (!File.Exists(filePath))
+            return false;
+        
+        var jsonStr = File.ReadAllText(filePath);
+
+        if (JsonConvert.DeserializeObject<T>(jsonStr) is { } deserializedData)
+            data = deserializedData;
+
+        return data != null;
+    }
+
+    public bool TrySerializeToFile(string filePath, T data)
+    {
+        if (JsonConvert.SerializeObject(data, Formatting.Indented) is not { } jsonStr)
+            return false;
+
+        var directory = Path.GetDirectoryName(filePath);
+        if (string.IsNullOrEmpty(directory))
+            return false;
+        
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+        
+        File.WriteAllTextAsync(filePath, jsonStr);
+        return true;
+    }
+}
