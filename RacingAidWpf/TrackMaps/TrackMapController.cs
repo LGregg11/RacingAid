835d810702f73ac522425f858674cb4588be7735
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-25 22:20:53 +0000

    Add track map controller

---
+++ RacingAidWpf/TrackMaps/TrackMapController.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using RacingAidWpf.FileHandlers;
+using RacingAidWpf.Resources;
+
+namespace RacingAidWpf.TrackMaps;
+
+public class TrackMapController
+{
+    private static readonly string TrackMapsJsonFullPath =
+        Path.Combine(Resource.DataDirectory, "TrackMaps.json");
+    
+    private readonly IHandleData<TrackMaps> trackMapDataHandler;
+    private readonly List<TrackMap> trackMaps = [];
+
+    public TrackMapController(IHandleData<TrackMaps> trackMapDataHandler)
+    {
+        this.trackMapDataHandler = trackMapDataHandler;
+        if (trackMapDataHandler.TryDeserializeFromFile(TrackMapsJsonFullPath, out var trackMapData))
+            trackMaps = trackMapData.Maps;
+    }
+    
+    public bool TryGetTrackMap(string trackName, out TrackMap? trackMap)
+    {
+        trackMap = trackMaps.FirstOrDefault(m => m.Name == trackName);
+        return trackMap != null;
+    }
+
+    public void SaveTrackMap(TrackMap trackMap)
+    {
+        if (trackMaps.Any(m => m.Name == trackMap.Name))
+            return;
+        
+        trackMaps.Add(trackMap);
+        if (!trackMapDataHandler.TrySerializeToFile(TrackMapsJsonFullPath, new TrackMaps(trackMaps)))
+        {
+            // TODO: Add error log here
+        }
+    }
+}
