6d2fd1d38500c868a029ed47e13cd78523e9bf3a
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-26 01:41:44 +0000

    Added base functionality required for creating and updating TrackMaps

--- RacingAidWpf/ViewModel/TrackMapOverlayViewModel.cs
+++ RacingAidWpf/ViewModel/TrackMapOverlayViewModel.cs
@@ -1,26 +1,92 @@
 ﻿using System.Windows;
+using RacingAidWpf.FileHandlers;
+using RacingAidWpf.Tracks;
 
 namespace RacingAidWpf.ViewModel;
 
 public class TrackMapOverlayViewModel : ViewModel
 {
-    private bool isTrackMapAvailable;
+    private readonly TrackMapController trackMapController;
+    
+    private readonly List<TrackMapPosition> currentTrackMapPositions = [];
 
-    public bool IsTrackMapAvailable
+    private bool isCurrentlyTrackingMap;
+    
+    private string currentTrackName;
+    private string CurrentTrackName
     {
-        get => isTrackMapAvailable;
+        get => currentTrackName;
         set
         {
-            if (isTrackMapAvailable == value)
+            if (currentTrackName == value)
                 return;
             
-            isTrackMapAvailable = value;
+            currentTrackName = value;
+            OnTrackUpdated();
+        }
+    }
+
+    private TrackMap currentTrackMap;
+    private TrackMap CurrentTrackMap
+    {
+        get => currentTrackMap;
+        set
+        {
+            if (currentTrackMap == value)
+                return;
+            
+            currentTrackMap = value;
             OnPropertyChanged();
+            OnPropertyChanged(nameof(IsTrackMapAvailable));
             OnPropertyChanged(nameof(TrackMapVisibility));
             OnPropertyChanged(nameof(NoTrackMapTextVisibility));
         }
     }
+    
+    public bool IsTrackMapAvailable => CurrentTrackMap != null;
 
     public Visibility TrackMapVisibility => IsTrackMapAvailable ? Visibility.Visible : Visibility.Collapsed;
     public Visibility NoTrackMapTextVisibility => !IsTrackMapAvailable ? Visibility.Visible : Visibility.Collapsed;
+
+    public TrackMapOverlayViewModel(TrackMapController trackMapController = null)
+    {
+        this.trackMapController = trackMapController ?? new TrackMapController(new JsonHandler<TrackMaps>());
+        
+        RacingAidUpdateDispatch.Update += OnUpdate;
+    }
+
+    private void OnUpdate()
+    {
+        CurrentTrackName = RacingAidSingleton.Instance.TrackData.TrackName;
+
+        if (CurrentTrackMap != null)
+        {
+            UpdateDriverPositionsOnTrack();
+            return;
+        }
+
+        if (isCurrentlyTrackingMap)
+            UpdateCurrentTrackMapPositions();
+    }
+
+    private void OnTrackUpdated()
+    {
+        if (trackMapController.TryGetTrackMap(CurrentTrackName, out var trackMap))
+        {
+            CurrentTrackMap = trackMap;
+            return;
+        }
+        
+        CurrentTrackMap = null;
+    }
+
+    private void UpdateDriverPositionsOnTrack()
+    {
+        // TODO
+    }
+    
+    private void UpdateCurrentTrackMapPositions()
+    {
+        // TODO
+    }
 }
