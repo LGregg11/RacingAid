21890bb0b98a53de7d2038c3d1c8de570539f54d
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-27 23:09:53 +0000

    Added functionality to get enums from the config

--- RacingAidWpf/Configuration/ConfigSection.cs
+++ RacingAidWpf/Configuration/ConfigSection.cs
@@ -4,7 +4,7 @@ namespace RacingAidWpf.Configuration;
 
 public class ConfigSection(IConfig config, string section)
 {
-    public event Action? ConfigUpdated;
+    public event Action ConfigUpdated;
     
     protected bool GetBool(string key, bool defaultValue = true)
     {
@@ -21,6 +21,16 @@ public class ConfigSection(IConfig config, string section)
             SetValue(key, defaultValue.ToString());
         
         return configValue ?? defaultValue;
+    }
+    
+    protected T GetEnum<T>(string key, T defaultValue) where T : struct, Enum
+    {
+        var defaultValueStr = Enum.GetName(typeof(T), defaultValue);
+        
+        if (!config.TryGetString(section, key, out var configValue))
+            SetValue(key, defaultValueStr);
+
+        return Enum.TryParse(configValue, out T parseValue) ? parseValue : defaultValue;
     }
     
     protected double GetDouble(string key, double defaultValue = 0d)
