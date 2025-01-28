91a3e37d55625f8cb1557094000b585da193c45f
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-01-28 01:06:04 +0000

    Added Enums.NET package ref & Added EnumEntryModel

--- RacingAidWpf/Model/SimulatorEntryModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using RacingAidData.Simulators;
-
-namespace RacingAidWpf.Model;
-
-public class SimulatorEntryModel(string simulatorName, Simulator simulatorType)
-{
-    public string SimulatorName { get; } = simulatorName;
-    public Simulator SimulatorType { get; } = simulatorType;
-}
