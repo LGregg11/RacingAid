1b8ea499955614c320e1e94600fe642a4a4d1ed9
Luke Gregory <L.Gregory11@hotmail.co.uk>
2025-02-04 00:41:07 +0000

    Fixed Log4NetLogger to updated log level on instantiation

--- RacingAidWpf/Logging/Log4NetLogger.cs
+++ RacingAidWpf/Logging/Log4NetLogger.cs
@@ -2,48 +2,71 @@
 
 namespace RacingAidWpf.Logging;
 
-public class Log4NetLogger(ILog log) : ILogger
+public class Log4NetLogger : ILogger
 {
+    private readonly ILog logger;
     private LogLevel logLevel;
+
+    public Log4NetLogger(ILog log)
+    {
+        logger = log;
+        logLevel = GetLogLevel(log);
+    }
 
     public void LogTrace(string message)
     {
         if (logLevel >= LogLevel.Trace)
-            log.Debug($"(TRACE) - {message}"); // Have to use debug for log4net
+            logger.Debug($"(TRACE) - {message}"); // Have to use debug for log4net
     }
 
     public void LogDebug(string message)
     {
         if (logLevel >= LogLevel.Debug)
-            log.Debug(message);
+            logger.Debug(message);
     }
 
     public void LogInformation(string message)
     {
         if (logLevel >= LogLevel.Info)
-            log.Info(message);
+            logger.Info(message);
     }
 
     public void LogWarning(string message)
     {
-        if (logLevel >= LogLevel.Info)
-            log.Warn(message);
+        if (logLevel >= LogLevel.Warn)
+            logger.Warn(message);
     }
 
     public void LogError(string message)
     {
         if (logLevel >= LogLevel.Error)
-            log.Error(message);
+            logger.Error(message);
     }
 
     public void LogFatal(string message)
     {
         if (logLevel >= LogLevel.Fatal)
-            log.Fatal(message);
+            logger.Fatal(message);
     }
 
     public void SetLogLevel(LogLevel level)
     {
         logLevel = level;
+    }
+
+    private static LogLevel GetLogLevel(ILog log)
+    {
+        if (log.IsDebugEnabled)
+            return LogLevel.Debug;
+        if (log.IsInfoEnabled)
+            return LogLevel.Info;
+        if (log.IsWarnEnabled)
+            return LogLevel.Warn;
+        if (log.IsErrorEnabled)
+            return LogLevel.Error;
+        if (log.IsFatalEnabled)
+            return LogLevel.Fatal;
+
+        return LogLevel.Trace;
     }
 }
