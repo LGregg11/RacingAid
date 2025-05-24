5b72e122cb01fafbf4f3f95475aae10f628f7593
Luke Gregory <lukegregory.wales@gmail.com>
2025-05-24 22:44:35 +0100

    Add GRPC project: Add telemetry proto, client & service

---
+++ RacingAidGrpc/TelemetryService.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Channels;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace RacingAidGrpc;
+
+public class TelemetryService : Telemetry.TelemetryBase
+{
+    private event EventHandler<bool> StatusUpdated;
+
+    public void UpdateStatus(bool status)
+    {
+        StatusUpdated?.Invoke(this, status);
+    }
+
+    public override async Task SubscribeToSessionStatus(
+        Empty _,
+        IServerStreamWriter<SessionStatusResponse> responseStream,
+        ServerCallContext context)
+    {
+        var channel = Channel.CreateUnbounded<bool>();
+        
+        StatusUpdated += async (_, active) =>
+        {
+            await channel.Writer.WriteAsync(active);
+        };
+
+        try
+        {
+            await foreach (var active in channel.Reader.ReadAllAsync(context.CancellationToken))
+            {
+                await responseStream.WriteAsync(new SessionStatusResponse { SessionActive = active });
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // Do nothing I guess
+        }
+        finally
+        {
+            channel.Writer.TryComplete(); // Close the channel when done
+        }
+    }
+}
---
+++ RacingAidGrpc/Utils.cs
@@ -0,0 +1,7 @@
+﻿namespace RacingAidGrpc;
+
+public static class Utils
+{
+    public static string DefaultHost => "127.0.0.1";
+    public static string DefaultPort => "5000";
+}
