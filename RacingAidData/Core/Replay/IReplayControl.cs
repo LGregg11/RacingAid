01fc999ad45c3f9aeb3ea42c0d05d7f3aa853b28
Luke Gregory <lukegregory.wales@gmail.com>
2025-05-31 18:02:42 +0100

    Add IReplayControl interface and implementation

---
+++ RacingAidData/Core/Replay/IReplayControl.cs
@@ -0,0 +1,20 @@
+﻿using RacingAidData.Core.Models;
+
+namespace RacingAidData.Core.Replay;
+
+public interface IReplayControl
+{
+    public event Action<RaceDataModel> ReplayDataReceived;
+    
+    public bool IsRecording { get; }
+    public bool IsReplaying { get; }
+    
+    public void StartRecording(string filePath);
+    public void RecordDataAsync(RaceDataModel data);
+    public Task StopRecordingAsync();
+
+    public IList<string> GetReplays();
+    public void SelectReplay(string filePath);
+    public void StartReplay();
+    public void StopReplay();
+}
---
+++ RacingAidData/Core/Replay/ReplayController.cs
@@ -0,0 +1,61 @@
+﻿namespace RacingAidData.Core.Replay;
+
+public class ReplayController
+{
+    private readonly IRecordData dataRecorder;
+    private readonly IReplayData dataReplayer;
+    
+    public bool IsRecording => dataRecorder.IsRecording;
+    public bool IsReplaying => dataReplayer.IsReplaying;
+
+    public ReplayController(IRecordData? dataRecorder = null, IReplayData? dataReplayer = null)
+    {
+        this.dataRecorder = dataRecorder ?? new DataRecorder();
+        this.dataReplayer = dataReplayer ?? new DataReplayer();
+    }
+
+    public void StartRecording(string filePath = "")
+    {
+        if (IsRecording || IsReplaying)
+            return;
+        
+        var directory = Path.GetDirectoryName(filePath) ?? string.Empty;
+        var fileName = Path.GetFileNameWithoutExtension(filePath);
+        
+        dataRecorder.Start(directory, fileName);
+    }
+
+    public async Task StopRecordingAsync()
+    {
+        if (!IsRecording || IsReplaying)
+            return;
+        
+        await dataRecorder.StopAsync();
+    }
+
+    public IList<string> GetReplays()
+    {
+        return new List<string>();
+    }
+
+    public void SelectReplay(string filePath)
+    {
+        dataReplayer.SetupReplay(filePath);
+    }
+
+    public void StartReplay()
+    {
+        if (IsRecording || IsReplaying)
+            return;
+        
+        dataReplayer.Start();
+    }
+
+    public void StopReplay()
+    {
+        if (IsRecording || !IsReplaying)
+            return;
+        
+        dataReplayer.Stop();
+    }
+}
